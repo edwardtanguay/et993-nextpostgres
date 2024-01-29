742e26cad641ebf28e41205dbdeb1329add329b1
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-29 14:05:40 +0100

    form looks good

--- src/components/NotesForm.tsx
+++ src/components/NotesForm.tsx
@@ -1,5 +1,25 @@
 export const NotesForm = () => {
 	return (
-		<p>Notes form</p>
+		<form className="bg-slate-400 p-6 rounded-lg">
+
+			<div className="mb-3">
+				<label className="block mb-1" htmlFor="note">Note:</label>
+				<textarea className="w-full" id="note"/>
+			</div>
+
+			<div className="mb-3">
+				<label className="block mb-1" htmlFor="rank">Rank:</label>
+				<input className="w-[4rem]" id="rank" type="text"/>
+			</div>
+
+			<div className="mb-3">
+				<label className="block mb-1" htmlFor="app_pin">PIN:</label>
+				<input className="w-[7rem]" type="password" id="app_pin"/>
+			</div>
+
+			<div className="mb-3 flex justify-end">
+				<button type="button" className="bg-slate-800 px-2 py-1 rounded-sm text-slate-400">Save</button>
+			</div>
+		</form>
 	)
 }
