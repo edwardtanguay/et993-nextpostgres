49dfbf97b88be6954e8b7b9640f589ffaa5f5b65
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-29 13:54:43 +0100

    centered/styled notes

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -6,10 +6,10 @@ interface IProps {
 
 export const Notes = ({notes} : IProps) => {
 	return (
-		<ul>
+		<ul className="mt-3">
 			{notes.map(note => {
 				return (
-					<li key={note.id}><span>{note.rank}</span>{note.body}</li>
+					<li key={note.id} className="flex gap-2 mb-2 items-center"><span className="bg-slate-700 p-1 rounded w-[2.5rem] flex justify-center text-yellow-400">{note.rank}</span><span className="flex"><span className="align-middle h-full">{note.body}</span></span></li>
 				)
 			})}
 		</ul>
